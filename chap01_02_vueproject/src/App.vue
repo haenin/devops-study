2c5c38560944f6af77e069719f75cbb4603ecf61
haenin <gpdnjsqhdld@naver.com>
2025-11-04 19:41:59 +0900

    docker study

---
+++ chap01_02_vueproject/src/App.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="plus">
+      <h1>덧셈 기능 만들기</h1>
+      <label>num1: </label><input type="text" v-model="num1">&nbsp;
+      <label>num2: </label><input type="text" v-model="num2">&nbsp;
+      <button @click="sendPlus">더하기</button>
+      <hr>
+      <p>{{ num1 }} + {{ num2 }} = {{ sum }}</p>
+  </div>
+</template>
+
+<script setup>
+  import {ref} from 'vue';
+
+  const num1 = ref(0);
+  const num2 = ref(0);
+  const sum = ref(0);
+
+  const sendPlus = async () => {
+
+    /* 1. 백엔드에서 CORS, 프론트에서 X */
+    // const response = await fetch(`http://localhost:7777/plus?num1=${num1.value}&num2=${num2.value}`);
+
+    /* 2. 백엔드에서 CORS, 프론트에서 X(백엔드만 컨테이너화) */
+    // const response = await fetch(`http://localhost:8888/plus?num1=${num1.value}&num2=${num2.value}`);
+
+    /* 3. 백엔드에서 X, 프론트에서 CORS(백엔드만 컨테이너화) */
+    // const response = await fetch(`http://localhost:5173/api/plus?num1=${num1.value}&num2=${num2.value}`);
+    
+    /* 이후 코드는 post 요청에 데이터는 request body를 활용(백엔드도 수정) */
+    // const response = await fetch(`http://localhost:5173/api/plus`, {
+    //   method: 'POST',
+    //   headers: {
+    //     'Content-Type': 'application/json;charset=utf-8;'
+    //   },
+    //   body: JSON.stringify({num1: num1.value, num2: num2.value})
+    // })
+
+    /* 4. 백엔드에서 CORS, 프론트에서 X(프론트와 백엔드 모두 컨테이너화) */
+    // const response = await fetch(`http://localhost:8055/plus`, {
+    //   method: 'POST',
+    //   headers: {
+    //     'Content-Type': 'application/json;charset=utf-8;'
+    //   },
+    //   body: JSON.stringify({num1: num1.value, num2: num2.value})
+    // })
+
+    /* 5. 백엔드에서 X, 프론트에서 CORS(프론트와 백엔드 모두 컨테이너화(docker-compose, bridge network 활용)) */
+    const response = await fetch(`http://localhost:8011/api/plus`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json;charset=utf-8;'
+      },
+      body: JSON.stringify({num1: num1.value, num2: num2.value})
+    })
+    
+    
+    const data = await response.json();
+    sum.value = data.sum;
+  }
+</script>
+
+<style scoped>
+  .plus {
+    text-align: center;
+  }
+</style>
