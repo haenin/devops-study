122712fba4d7810dbaa23afbaff261e2d8e5f964
haenin <gpdnjsqhdld@naver.com>
2025-11-06 12:40:48 +0900

    docker study

---
+++ chap01_01_bootproject/src/main/java/com/haenin/bootproject/service/CalculatorService.java
@@ -0,0 +1,11 @@
+package com.haenin.bootproject.service;
+
+import com.haenin.bootproject.dto.CalculatorDTO;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CalculatorService {
+    public int plusTwoNumbers(CalculatorDTO calculatorDTO) {
+        return calculatorDTO.getNum1() + calculatorDTO.getNum2();
+    }
+}
---
+++ chap01_01_bootproject/src/test/java/com/haenin/bootproject/Chap0101BootprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haenin.bootproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chap0101BootprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
